1038672c662bd3fb9c9d1a2a78ec0a81642d37d9
Marc Gravel <gravel.m@outlook.com>
2021-07-08 21:54:00 -0600

    Array Of Tweets

---
+++ js/app.js
@@ -0,0 +1,71 @@
+var tweetobjects = [
+    {
+        "tweet": "YANKEE DOODLE: *sticks feather in his cap* This is called macaroni.. YANKEE DOODLE'S FRIEND: Ok, cool. Listen man, everybodys worried about you.",
+        "username": "@tastefactory",
+        "created_at": "10-03-2015",
+        "age": 21
+    },
+    {
+        "tweet": "Sick of having to go to 2 different huts to buy p;izza and sunglasses.",
+        "username": "@Leemanish",
+        "created_at": "03-24-2013",
+        "age": 25
+    },
+    {
+        "tweet": "BUILDING INSPECTOR: Whats this called? DARTH VADER: The death- [inspectors eyes look up from his clipboard] DARTH VADER: Uh, the health star.",
+        "username": "@bobvulfov",
+        "created_at": "10-14-2015",
+        "age": 31
+    },
+    {
+        "tweet": "How dare you call me mentally unstable, on this, the day of my cats quinceanera.",
+        "username": "@phranqueigh",
+        "created_at": "05-28-2015",
+        "age": 29
+    },
+    {
+        "tweet": "ME: Can you dust my wets? SERVER: you can just ask for parmesan cheese ME: *confused lifting all of my spaghetti with my hands* please. My wets.",
+        "username": "@horsedivorce",
+        "created_at": "04-05-2019",
+        "age": 36
+    },
+    {
+        "tweet": "When I find myself in times of trouble, brother Mario comes to me. Speaking words of wisdom, 'it's a me'.",
+        "username": "@ellle_em",
+        "created_at": "08-15-2018",
+        "age": 32
+    },
+    {
+        "tweet": "*joins a conga line* ME: I can leave any time I like. *someone joins behind* ME: oh no",
+        "username": "@HansGrubertron",
+        "created_at": "03-10-2019",
+        "age": 23
+    },
+    {
+        "tweet": "The most cutting thing you can say is 'who's this clown?' because it implies they're a) a clown & b) not even one of the better-known clowns",
+        "username": "@skullmandible",
+        "created_at": "12-12-2013",
+        "age": 28
+    },
+    {
+        "tweet": "'Anybody named Jeff?' JEFF: Yes. GEOFF: Yeos.",
+        "username": "@mtobey",
+        "created_at": "01-20-2016",
+        "age": 52
+    },
+    {
+        "tweet": "How much for the horse tornado? Sir, thats a carousel.. I must have it.",
+        "username": "@neonsinatra",
+        "created_at": "02-26-2013",
+        "age": 45
+    }
+]
+
+var arrayLength = tweetobjects.length;
+
+for(var i = 0; i < arrayLength; i++) {
+    console.log(tweetobjects[i].tweet);
+    console.log(tweetobjects[i].username);
+    console.log(tweetobjects[i].created_at);
+    console.log();
+}
